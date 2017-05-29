6a0a95ca606f8f3e943eed5ef82566b0f617a614
MeaslyPickings <illogicaljp_211@hotmail.com>
2017-05-28 22:43:10 -0300

    a2-stage2 commit

--- app/src/main/java/com/example/hugh/passwordvalidator/PassValidator.java
+++ app/src/main/java/com/example/hugh/passwordvalidator/PassValidator.java
@@ -18,6 +18,33 @@ public class PassValidator {
         if (s.length() >= 8 && !s.contains(" ")){
             result++;
         }
+        if (s.contains("!") || s.contains("@") || s.contains("#")
+                || s.contains("$") || s.contains("%") || s.contains("^")
+                || s.contains("&") || s.contains("*") || s.contains("(")
+                || s.contains(")") || s.contains("_") || s.contains("+")
+                || s.contains("=")) {
+            result++;
+        }
+        if (s.contains("0") || s.contains("1") || s.contains("2")
+                || s.contains("3") || s.contains("4") || s.contains("5")
+                || s.contains("6") || s.contains("7") || s.contains("8")
+                || s.contains("9")){
+            result++;
+        }
+        char[] characters = s.toCharArray();
+        boolean upper = false;
+        boolean lower = false;
+        for (int i=0; i<characters.length; i++) {
+            if (Character.isUpperCase(characters[i]) == true) {
+                upper = true;
+            }
+            else if (Character.isLowerCase(characters[i]) == true) {
+                lower = true;
+            }
+        }
+        if (upper == true && lower == true) {
+            result++;
+        }
         return result;
     }
 }
--- app/src/test/java/com/example/hugh/passwordvalidator/PassTest.java
+++ app/src/test/java/com/example/hugh/passwordvalidator/PassTest.java
@@ -21,12 +21,32 @@ public class PassTest {
 
     @Test
     public void isNotPassword() {
+
         assertNotEquals(check.validate(string), 0);
     }
 
     @Test
     public void has8Characters() {
+
         assertNotEquals(check.validate(string), 1);
+    }
+
+    @Test
+    public void hasSpecialChar() {
+
+        assertNotEquals(check.validate(string), 2);
+    }
+
+    @Test
+    public void hasDigit() {
+
+        assertNotEquals(check.validate(string), 3);
+    }
+
+    @Test
+    public void hasDifCase() {
+
+        assertNotEquals(check.validate(string), 4);
     }
 
 }
